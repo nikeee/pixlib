6b2c2ba9861ebcc0c08c8fc48976ba9fea70e5c6
nikeee13 <nikeee@outlook.com>
2013-05-10 16:57:06 +0200

    added radial scanner

---
+++ System.Drawing.Analysis/Manipulation/RadialScanner.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+
+namespace System.Drawing.Analysis.Manipulation
+{
+    public class RadialScanner : IPixelScanner
+    {
+        public Rectangle View { get; set; }
+
+        private readonly IGetPixelProvider _provider;
+
+        #region Ctors
+
+        public RadialScanner(IGetPixelProvider provider)
+        {
+            if (provider == null)
+                throw new ArgumentNullException("provider");
+            _provider = provider;
+        }
+
+        #endregion
+
+        public IEnumerable<Point> FindPixels(Color color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Point First(Color color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Point FirstOrDefault(Color color)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
