01194d31aff3134433f039cf808d0dd9d41c215e
nikeee13 <nikeee@outlook.com>
2013-05-11 20:43:14 +0200

    added testing project

---
+++ System.Drawing.Analysis.Testing/SlowBitmalPixelProviderUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Drawing.Analysis.Testing
+{
+    [TestClass]
+    public class SlowBitmalPixelProviderUnitTest
+    {
+        [TestMethod]
+        public void TestSlowBitmalPixelProvider()
+        {
+
+        }
+    }
+}
