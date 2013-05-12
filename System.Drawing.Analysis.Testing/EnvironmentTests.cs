37e7807959360b005ae1aa76b42ace4ddd97c364
nikeee13 <nikeee@outlook.com>
2013-05-12 20:11:27 +0200

    added EnvironmentTests

---
+++ System.Drawing.Analysis.Testing/EnvironmentTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Drawing.Analysis.Testing
+{
+    [TestClass]
+    public class EnvironmentTests
+    {
+        [TestMethod]
+        [TestCategory("Environment")]
+        public void CheckVirtualScreen()
+        {
+            Assert.AreEqual(Windows.Forms.SystemInformation.VirtualScreen, Environment.VirtualScreen);
+        }
+        [TestMethod]
+        [TestCategory("Environment")]
+        public void CheckPrimaryMonitorSize()
+        {
+            Assert.AreEqual(Windows.Forms.SystemInformation.PrimaryMonitorSize, Environment.PrimaryMonitorSize);
+        }
+    }
+}
