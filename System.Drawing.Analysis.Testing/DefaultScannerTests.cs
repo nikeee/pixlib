5e5ea6c561afb885b0ac8e92d4e7ec75222ac9d9
nikeee13 <nikeee@outlook.com>
2013-05-12 19:53:52 +0200

    adding unit tests

--- System.Drawing.Analysis.Testing/DefaultScannerTests.cs
+++ System.Drawing.Analysis.Testing/DefaultScannerTests.cs
@@ -12,9 +12,10 @@ namespace System.Drawing.Analysis.Testing
         public void TestSimple()
         {
             var testBitmap = TestingHelper.GetTestBitmap();
-            using(var provider = new SlowBitmapPixelProvider(testBitmap))
+            using (var provider = new SlowBitmapPixelProvider(testBitmap))
             {
                 var scanner = new DefaultScanner(provider);
+
             }
         }
 
@@ -24,15 +25,55 @@ namespace System.Drawing.Analysis.Testing
         public void TestSimpleAny()
         {
             var testBitmap = TestingHelper.GetTestBitmap();
-            using(var provider = new SlowBitmapPixelProvider(testBitmap))
+            using (var provider = new SlowBitmapPixelProvider(testBitmap))
             {
                 var scanner = new DefaultScanner(provider);
 
-                bool expected = true; // there is exactly one white pixel
                 bool actual = scanner.Any(Color.White);
+                Assert.AreEqual(true, actual);
 
+                actual = scanner.Any(Color.FromArgb(255, 255, 255, 255));
+                Assert.AreEqual(true, actual);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Scanner")]
+        [TestCategory("DefaultScanner")]
+        public void TestSimpleForEach()
+        {
+            var testBitmap = TestingHelper.GetTestBitmap();
+            using (var provider = new SlowBitmapPixelProvider(testBitmap))
+            {
+                var scanner = new DefaultScanner(provider);
+                var counter = 0;
+                scanner.ForEach((x, y, c) => ++counter);
+
+                int expected = testBitmap.Width * testBitmap.Height;
+                int actual = counter;
                 Assert.AreEqual(expected, actual);
+            }
+        }
 
+        [TestMethod]
+        [TestCategory("Scanner")]
+        [TestCategory("DefaultScanner")]
+        public void TestViewForEach()
+        {
+            var testBitmap = TestingHelper.GetTestBitmap();
+            using (var provider = new SlowBitmapPixelProvider(testBitmap))
+            {
+                var scanner = new DefaultScanner(provider)
+                                  {
+                                      View = new Rectangle(3, 3, 2, 2)
+                                  };
+
+                var counter = 0;
+                scanner.ForEach((x, y, c) => ++counter);
+
+                int expected = scanner.View.Width * scanner.View.Height;
+                int actual = counter;
+                Assert.AreEqual(expected, actual);
             }
         }
     }
