ab534417909263e99f35155d4b1af9743cf148d2
nikeee13 <nikeee@outlook.com>
2013-05-12 17:03:57 +0200

    implemented SetPixel testing methods

--- System.Drawing.Analysis.Testing/SlowBitmalPixelProviderUnitTest.cs
+++ System.Drawing.Analysis.Testing/SlowBitmalPixelProviderUnitTest.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Drawing;
-using System.Drawing.Analysis;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace System.Drawing.Analysis.Testing
 {
