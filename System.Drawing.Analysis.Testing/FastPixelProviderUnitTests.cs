9d7942a2c5540979a2db004bbb6012beb0ee0d90
nikeee13 <nikeee@outlook.com>
2013-05-12 17:03:36 +0200

    implemented SetPixel testing methods

--- System.Drawing.Analysis.Testing/FastPixelProviderUnitTests.cs
+++ System.Drawing.Analysis.Testing/FastPixelProviderUnitTests.cs
@@ -46,5 +46,57 @@ namespace System.Drawing.Analysis.Testing
                 }
             }
         }
+
+        [TestMethod]
+        public void SetPixelFast()
+        {
+            var testBitmap = TestingHelper.GetTestBitmap();
+            using (var testBitmapUnlocked = TestingHelper.GetTestBitmap())
+            {
+                using (var fast = new FastBitmapPixelProvider(testBitmap, true))
+                {
+                    for (int x = 0; x < testBitmap.Width; ++x)
+                    {
+                        for (int y = 0; y < testBitmap.Height; ++y)
+                        {
+                            Color expected = TestingHelper.GetRandomColor();
+                            testBitmapUnlocked.SetPixel(x, y, expected);
+                            fast.SetPixel(x, y, expected);
+
+                            var actualNative = testBitmapUnlocked.GetPixel(x, y);
+                            var actualFast = fast.GetPixel(x, y);
+                            Assert.AreEqual(expected, actualNative);
+                            Assert.AreEqual(expected, actualFast);
+                        }
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void SetPixelFast2()
+        {
+            var testBitmap = TestingHelper.GetTestBitmap2();
+            using (var testBitmapUnlocked = TestingHelper.GetTestBitmap2())
+            {
+                using (var fast = new FastBitmapPixelProvider(testBitmap, true))
+                {
+                    for (int x = 0; x < testBitmap.Width; ++x)
+                    {
+                        for (int y = 0; y < testBitmap.Height; ++y)
+                        {
+                            Color expected = TestingHelper.GetRandomColor();
+                            testBitmapUnlocked.SetPixel(x, y, expected);
+                            fast.SetPixel(x, y, expected);
+
+                            var actualNative = testBitmapUnlocked.GetPixel(x, y);
+                            var actualFast = fast.GetPixel(x, y);
+                            Assert.AreEqual(expected, actualNative);
+                            Assert.AreEqual(expected, actualFast);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
