36bc0f0da99a9613a498e477ab2973b86465a31a
nikeee13 <nikeee@outlook.com>
2013-05-16 16:44:22 +0200

    adding XML-documentation

--- System.Drawing.Analysis/ColorToleranceBorders.cs
+++ System.Drawing.Analysis/ColorToleranceBorders.cs
@@ -63,5 +63,65 @@ namespace System.Drawing.Analysis
             _maxG = (baseColor.G + tolerance.G > 255) ? 255 : (baseColor.G + tolerance.G);
             _maxB = (baseColor.B + tolerance.B > 255) ? 255 : (baseColor.B + tolerance.B);
         }
+
+        #region equals
+
+        /// <summary>Determines whether this instance and another specified <see cref="T:ColorToleranceBorders"/> object have the same values.</summary>
+        /// <param name="obj">The other <see cref="T:ColorToleranceBorders"/> instance.</param>
+        /// <returns>true if the values of the <paramref name="obj"/> parameter are the same as this instance; otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            var other = (ColorToleranceBorders)obj;
+            return _minA == other._minA
+                && _minR == other._minR
+                && _minG == other._minG
+                && _minB == other._minB
+                && _maxA == other._maxA
+                && _maxR == other._maxR
+                && _maxG == other._maxG
+                && _maxB == other._maxB
+                && _baseColor.ValuesEqual(other._baseColor)
+                && _baseTolerance == other._baseTolerance;
+        }
+
+        /// <summary>Returns the hash code for this <see cref="T:ColorToleranceBorders"/>.</summary>
+        /// <returns>The hash code for this <see cref="T:ColorToleranceBorders"/></returns>
+        public override int GetHashCode()
+        {
+            return (_minA ^ _minR ^ _minG ^ _minB) ^ (_maxA ^ _maxR ^ _maxG ^ _maxB) ^ _baseColor.GetHashCode();
+        }
+
+        #endregion
+        #region ==-operator
+
+        /// <summary>Determines whether two instances of <see cref="T:ColorToleranceBorders"/> objects have the same values.</summary>
+        /// <param name="toleranceBorder1">The first <see cref="T:ColorToleranceBorders"/> instance.</param>
+        /// <param name="toleranceBorder2">The second <see cref="T:ColorToleranceBorders"/> instance.</param>
+        /// <returns>true if the values of the given instances have the same values; otherwise, false.</returns>
+        public static bool operator ==(ColorToleranceBorders toleranceBorder1, ColorToleranceBorders toleranceBorder2)
+        {
+            // In case of refactoring to class, take care of this!
+            return toleranceBorder1._minA == toleranceBorder2._minA
+                && toleranceBorder1._minR == toleranceBorder2._minR
+                && toleranceBorder1._minG == toleranceBorder2._minG
+                && toleranceBorder1._minB == toleranceBorder2._minB
+                && toleranceBorder1._maxA == toleranceBorder2._maxA
+                && toleranceBorder1._maxR == toleranceBorder2._maxR
+                && toleranceBorder1._maxG == toleranceBorder2._maxG
+                && toleranceBorder1._maxB == toleranceBorder2._maxB
+                && toleranceBorder1._baseColor.ValuesEqual(toleranceBorder2._baseColor)
+                && toleranceBorder1._baseTolerance == toleranceBorder2._baseTolerance;
+        }
+
+        /// <summary>Determines whether two instances of <see cref="T:ColorToleranceBorders"/> objects do not have the same values.</summary>
+        /// <param name="toleranceBorder1">The first <see cref="T:ColorToleranceBorders"/> instance.</param>
+        /// <param name="toleranceBorder2">The second <see cref="T:ColorToleranceBorders"/> instance.</param>
+        /// <returns>true if the values of the given instances do not have the same values; otherwise, false.</returns>
+        public static bool operator !=(ColorToleranceBorders toleranceBorder1, ColorToleranceBorders toleranceBorder2)
+        {
+            return !(toleranceBorder1 == toleranceBorder2);
+        }
+
+        #endregion
     }
 }
