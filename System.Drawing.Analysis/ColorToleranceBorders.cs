4d7e83f267a2c0e6c524d7293c1afa848e571941
nikeee13 <nikeee@outlook.com>
2013-05-16 16:11:46 +0200

    added some XML-documentation

--- System.Drawing.Analysis/ColorToleranceBorders.cs
+++ System.Drawing.Analysis/ColorToleranceBorders.cs
@@ -9,27 +9,37 @@ namespace System.Drawing.Analysis
     public struct ColorToleranceBorders
     {
         private readonly int _minA;
+        private readonly int _minR;
+        private readonly int _minG;
+        private readonly int _minB;
+
+        /// <summary>Gets the minimum alpha value the the a can have to match the <see cref="T:ColorTolerance"/>.</summary>
         public int MinA { get { return _minA; } }
 
-        private readonly int _minR;
+        /// <summary>Gets the minimum red value the the a can have to match the <see cref="T:ColorTolerance"/>.</summary>
         public int MinR { get { return _minR; } }
 
-        private readonly int _minG;
+        /// <summary>Gets the minimum green value the the a can have to match the <see cref="T:ColorTolerance"/>.</summary>
         public int MinG { get { return _minG; } }
 
-        private readonly int _minB;
+        /// <summary>Gets the minimum blue value the the a can have to match the <see cref="T:ColorTolerance"/>.</summary>
         public int MinB { get { return _minB; } }
 
         private readonly int _maxA;
+        private readonly int _maxR;
+        private readonly int _maxG;
+        private readonly int _maxB;
+
+        /// <summary>Gets the maximum alpha value the the a can have to match the <see cref="T:ColorTolerance"/>.</summary>
         public int MaxA { get { return _maxA; } }
 
-        private readonly int _maxR;
+        /// <summary>Gets the maximum red value the the a can have to match the <see cref="T:ColorTolerance"/>.</summary>
         public int MaxR { get { return _maxR; } }
 
-        private readonly int _maxG;
+        /// <summary>Gets the maximum green value the the a can have to match the <see cref="T:ColorTolerance"/>.</summary>
         public int MaxG { get { return _maxG; } }
 
-        private readonly int _maxB;
+        /// <summary>Gets the maximum blue value the the a can have to match the <see cref="T:ColorTolerance"/>.</summary>
         public int MaxB { get { return _maxB; } }
 
         private Color _baseColor;
