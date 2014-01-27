6bd749a3a073ed21f0ac8c1b0192bc45cabd7c9f
unknown <nikeee@outlook.com>
2014-01-27 11:37:34 +0100

    Add custom color struct

---
+++ src/System.Drawing.Analysis/Color.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Drawing.Analysis
+{
+  public  struct Color
+    {
+        private byte _b;
+        private byte _g;
+        private byte _r;
+        private byte _a
+        private int _bgra;
+
+
+
+    }
+}
