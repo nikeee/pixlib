cecefb4246890b30a9cc9eb762616ea0e3076f56
nikeee13 <nikeee@outlook.com>
2014-01-28 22:56:27 +0100

    Add Documentation

--- src/System.Drawing.Analysis/NativeColor.cs
+++ src/System.Drawing.Analysis/NativeColor.cs
@@ -17,27 +17,57 @@ namespace System.Drawing.Analysis
         [FieldOffset(0)]
         private int _bgra;
 
+        /// <summary>
+        /// Gets or sets the alpha component value of this <see cref="T:System.Drawing.Analysis.NativeColor"/> structure.
+        /// </summary>
         public byte A { get { return _a; } set { _a = value; } }
+
+        /// <summary>
+        /// Gets or sets the red component value of this <see cref="T:System.Drawing.Analysis.NativeColor"/> structure.
+        /// </summary>
         public byte R { get { return _r; } set { _r = value; } }
+
+        /// <summary>
+        /// Gets or sets the green component value of this <see cref="T:System.Drawing.Analysis.NativeColor"/> structure.
+        /// </summary>
         public byte G { get { return _g; } set { _g = value; } }
+
+        /// <summary>
+        /// Gets or sets the blue component value of this <see cref="T:System.Drawing.Analysis.NativeColor"/> structure.
+        /// </summary>
         public byte B { get { return _b; } set { _b = value; } }
+        
+        /// <summary>
+        /// Gets or sets all component values of this <see cref="T:System.Drawing.Analysis.NativeColor"/> structure.
+        /// </summary>
         public int Bgra { get { return _bgra; } set { _bgra = value; } }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "b")]
-        public NativeColor(byte a, byte r, byte g, byte b)
+        /// <summary>Creates a <see cref="T:System.Drawing.Analysis.NativeColor" /> structure from the four ARGB component (alpha, red, green, and blue) values.</summary>
+        /// <param name="alpha">The alpha component.</param>
+        /// <param name="red">The red component.</param>
+        /// <param name="green">The green component.</param>
+        /// <param name="blue">The blue component.</param>
+        /// <returns>A new instance of <see cref="T:System.Drawing.Analysis.NativeColor" />.</returns>
+        public NativeColor(byte alpha, byte red, byte green, byte blue)
         {
             _bgra = 0;
-            _a = a;
-            _r = r;
-            _g = g;
-            _b = b;
+            _a = alpha;
+            _r = red;
+            _g = green;
+            _b = blue;
         }
+        /// <summary>Creates a <see cref="T:System.Drawing.Analysis.NativeColor" /> structure from a 32-bit ARGB value.</summary>
+        /// <param name="bgra">A value specifying the 32-bit BGRA value. </param>
+        /// <returns>A new instance of <see cref="T:System.Drawing.Analysis.NativeColor" />.</returns>
         public NativeColor(int bgra)
         {
             _a = _r = _b = _g = 0;
             _bgra = bgra;
         }
 
+        /// <summary>Creates a <see cref="T:System.Drawing.Analysis.NativeColor" /> structure from a 32-bit ARGB value.</summary>
+        /// <param name="argb">A value specifying the 32-bit ARGB value. </param>
+        /// <returns>The <see cref="T:System.Drawing.Analysis.NativeColor" /> structure that this method creates.</returns>
         public static NativeColor FromArgb(int argb)
         {
             return new NativeColor((byte)(argb >> 24), (byte)(argb >> 16), (byte)(argb >> 8), (byte)argb);
@@ -52,6 +82,10 @@ namespace System.Drawing.Analysis
             return _bgra;
         }
 
+        /// <summary>
+        /// Gets the 32-bit ARGB value of this <see cref="T:System.Drawing.Analysis.NativeColor"/> structure.
+        /// </summary>
+        /// <returns>The 32-bit ARGB value of this <see cref="T:System.Drawing.Analysis.NativeColor"/>.</returns>
         public int ToArgb()
         {
             return (_a << 24) | (_r << 16) | (_g << 8) | (_b << 0);
@@ -88,21 +122,33 @@ namespace System.Drawing.Analysis
     public static class Colors
     {
         private static readonly NativeColor _white = new NativeColor(0xFF, 0xFF, 0xFF, 0xFF);
+        /// <summary>Gets a system-defined color that has an ARGB value of #FFFFFFFF.</summary>
+        /// <returns>A <see cref="T:System.Drawing.Analysis.NativeColor" /> representing a system-defined color.</returns>
         public static NativeColor White { get { return _white; } }
 
         private static readonly NativeColor _black = new NativeColor(0xFF, 0x0, 0x0, 0x0);
+        /// <summary>Gets a system-defined color that has an ARGB value of #FF000000.</summary>
+        /// <returns>A <see cref="T:System.Drawing.Analysis.NativeColor" /> representing a system-defined color.</returns>
         public static NativeColor Black { get { return _black; } }
 
         private static readonly NativeColor _transparent = new NativeColor(0x0, 0x0, 0x0, 0x0);
+        /// <summary>Gets a system-defined color that has an ARGB value of #00000000.</summary>
+        /// <returns>A <see cref="T:System.Drawing.Analysis.NativeColor" /> representing a system-defined color.</returns>
         public static NativeColor Transparent { get { return _transparent; } }
 
         private static readonly NativeColor _red = new NativeColor(0xFF, 0xFF, 0x0, 0x0);
+        /// <summary>Gets a system-defined color that has an ARGB value of #FFFF0000.</summary>
+        /// <returns>A <see cref="T:System.Drawing.Analysis.NativeColor" /> representing a system-defined color.</returns>
         public static NativeColor Red { get { return _red; } }
 
         private static readonly NativeColor _green = new NativeColor(0xFF, 0x0, 0xFF, 0x0);
+        /// <summary>Gets a system-defined color that has an ARGB value of #FF00FF00.</summary>
+        /// <returns>A <see cref="T:System.Drawing.Analysis.NativeColor" /> representing a system-defined color.</returns>
         public static NativeColor Green { get { return _green; } }
 
         private static readonly NativeColor _blue = new NativeColor(0xFF, 0x0, 0x0, 0xFF);
+        /// <summary>Gets a system-defined color that has an ARGB value of #FF0000FF.</summary>
+        /// <returns>A <see cref="T:System.Drawing.Analysis.NativeColor" /> representing a system-defined color.</returns>
         public static NativeColor Blue { get { return _blue; } }
     }
 }
