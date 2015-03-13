3ecf4330db58ab04e301ab70f7994d3a3e2ba4db
Niklas Mollenhauer <nikeee@outlook.com>
2015-03-13 01:07:31 +0100

    Migrating to NUnit

--- src/System.Drawing.Analysis.Testing/SlowBitmalPixelProviderUnitTest.cs
+++ src/System.Drawing.Analysis.Testing/SlowBitmalPixelProviderUnitTest.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 
 namespace System.Drawing.Analysis.Testing
 {
-    [TestClass]
+    [TestFixture]
     public class SlowPixelProviderTests
     {
-        [TestMethod]
-        [TestCategory("PixelProvider")]
-        [TestCategory("Slow")]
+        [Test]
+        [Category("PixelProvider")]
+        [Category("Slow")]
         public void GetPixelSlow()
         {
             var testBitmap = TestingHelper.GetTestBitmap();
@@ -20,15 +20,15 @@ namespace System.Drawing.Analysis.Testing
                     {
                         var expected = NativeColor.FromDrawingColor(testBitmap.GetPixel(x, y));
                         var actual = slow.GetPixel(x, y);
-                        Assert.AreEqual<NativeColor>(expected, actual);
+                        AssertEx.AreEqual<NativeColor>(expected, actual);
                     }
                 }
             }
         }
 
-        [TestMethod]
-        [TestCategory("PixelProvider")]
-        [TestCategory("Slow")]
+        [Test]
+        [Category("PixelProvider")]
+        [Category("Slow")]
         public void GetPixelSlow2()
         {
             var testBitmap = TestingHelper.GetTestBitmap2();
@@ -40,15 +40,15 @@ namespace System.Drawing.Analysis.Testing
                     {
                         var expected = NativeColor.FromDrawingColor(testBitmap.GetPixel(x, y));
                         var actual = slow.GetPixel(x, y);
-                        Assert.AreEqual<NativeColor>(expected, actual);
+                        AssertEx.AreEqual<NativeColor>(expected, actual);
                     }
                 }
             }
         }
 
-        [TestMethod]
-        [TestCategory("PixelProvider")]
-        [TestCategory("Slow")]
+        [Test]
+        [Category("PixelProvider")]
+        [Category("Slow")]
         public void SetPixelSlow()
         {
             var testBitmap = TestingHelper.GetTestBitmap();
@@ -64,16 +64,16 @@ namespace System.Drawing.Analysis.Testing
                         NativeColor actual = slow.GetPixel(x, y);
                         NativeColor actual2 = NativeColor.FromDrawingColor(testBitmap.GetPixel(x, y));
 
-                        Assert.AreEqual<NativeColor>(expected, actual2);
-                        Assert.AreEqual<NativeColor>(expected, actual);
+                        AssertEx.AreEqual<NativeColor>(expected, actual2);
+                        AssertEx.AreEqual<NativeColor>(expected, actual);
                     }
                 }
             }
         }
 
-        [TestMethod]
-        [TestCategory("PixelProvider")]
-        [TestCategory("Slow")]
+        [Test]
+        [Category("PixelProvider")]
+        [Category("Slow")]
         public void SetPixelSlow2()
         {
             var testBitmap = TestingHelper.GetTestBitmap2();
@@ -85,18 +85,18 @@ namespace System.Drawing.Analysis.Testing
                     {
                         NativeColor expected = TestingHelper.GetRandomColor();
                         slow.SetPixel(x, y, expected);
-                        
+
                         NativeColor actual = slow.GetPixel(x, y);
 
-                        Assert.AreEqual<NativeColor>(expected, actual);
+                        AssertEx.AreEqual<NativeColor>(expected, actual);
                     }
                 }
             }
         }
 
-        [TestMethod]
-        [TestCategory("PixelProvider")]
-        [TestCategory("Slow")]
+        [Test]
+        [Category("PixelProvider")]
+        [Category("Slow")]
         public void SwapPixelSlow()
         {
             var testBitmap = TestingHelper.GetTestBitmap();
@@ -110,7 +110,7 @@ namespace System.Drawing.Analysis.Testing
 
                         var expected = slow.GetPixel(x, y);
                         var actual = slow.SwapPixel(x, y, c);
-                        Assert.AreEqual<NativeColor>(expected, actual);
+                        AssertEx.AreEqual<NativeColor>(expected, actual);
                     }
                 }
             }
